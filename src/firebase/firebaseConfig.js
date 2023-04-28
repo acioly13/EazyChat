685f53cf9368749b14cc40b3472b3deb493279c1
João Acioly <joao.acioly@dmslog.com>
2023-04-28 13:22:33 -0300

    hosting

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,8 +1,8 @@
 export default {
-    apiKey: "AIzaSyCnGmeUNJIbyh6KYHGEpTwV2hgRotuPa5A",
-    authDomain: "eazychat-58478.firebaseapp.com",
-    projectId: "eazychat-58478",
-    storageBucket: "eazychat-58478.appspot.com",
-    messagingSenderId: "1024871169061",
-    appId: "1:1024871169061:web:0f1b570ec0b05756a213cc"
+  apiKey: "AIzaSyD9MmTEtpIrEd7NfAkHbgnX8mIcuobyC1A",
+  authDomain: "eazychat-vass.firebaseapp.com",
+  projectId: "eazychat-vass",
+  storageBucket: "eazychat-vass.appspot.com",
+  messagingSenderId: "228715601014",
+  appId: "1:228715601014:web:d850b4c6ebd5b0d4f05357"
   };
